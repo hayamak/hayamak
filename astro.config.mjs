5d651c96388a2dde38140b65e3882cecd98457cb
hayamakazuyuki <hayama@mtg.biglobe.ne.jp>
2026-07-19 08:58:39 +0000

    ✅ astro.config.mjs updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://hayama.me",
 
+  output: "server",
+
   adapter: cloudflare({
     imageService: "compile",
   }),
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,24 @@
 // src/actions/index.ts
+
+import { defineAction } from "astro:actions";
+import { z } from "astro/zod";
+
+export const server = {
+  contact: defineAction({
+    input: z.object({
+      name: z.string().trim().max(100),
+      email: z.email(),
+      message: z.string().trim().min(1),
+    }),
+
+    accept: "form",
+    handler: async ({ name, email, message }) => {
+      console.log({
+        name,
+        email,
+        message,
+      });
+      return { success: true };
+    },
+  }),
+};
